06561ea1bcf9ff27d77b2e5fb4560b8742a3e512
Justine Guymard <justine.gmrd@gmail.com>
2020-07-16 16:35:57 +0200

    initialisation

---
+++ src/test/java/fr/inti/ApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.inti;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
