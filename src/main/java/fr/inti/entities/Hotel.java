b310b96f6f8dd64e635a2a299d6cebbbc3ea0a3d
Justine Guymard <justine.gmrd@gmail.com>
2020-07-21 17:18:25 +0200

    entities

--- src/main/java/fr/inti/entities/Hotel.java
+++ src/main/java/fr/inti/entities/Hotel.java
@@ -2,6 +2,16 @@ package fr.inti.entities;
 
 public class Hotel {
 	
+	private String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
 	
 
 }
